693bd400b0ca73a43c96a8a432f70e72c3d3352c
Swetha Kundaram <swethakundaram@gmail.com>
2019-09-03 15:49:56 +0530

    note and tip package

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -125,10 +125,10 @@ const siteConfig = {
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
 	
-/*	 markdownPlugins: [
+ markdownPlugins: [
     // Highlight admonitions.
     require('remarkable-admonitions')({ icon: 'svg-inline' })
-	] */
+	]
     
 };
 
