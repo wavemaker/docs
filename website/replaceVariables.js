46eb162bc3e0c5e8f583c30e2e33899240f44b5d
Sharath Chandra <sharathchandra0033@gmail.com>
2019-12-05 16:25:49 +0530

    Sed GNU issue fix

--- website/replaceVariables.js
+++ website/replaceVariables.js
@@ -11,7 +11,7 @@ console.log('Variable replacement started');
  * The exec function will run after the build directory is generated and it looks for notation #{<variable name>}#
  * in all the subdirectories and files and replaces the respective variable using the siteVariables object
  */
-exec("cd ./build/learn && LC_ALL=C find . -type f -name '*.*' -exec sed -i '' -e 's/#{wmVersionNumber}#/"+siteVariables['wmVersionNumber']+"/g' {} \\;", (err, stdout, stderr) => {
+exec("cd ./build/learn && LC_ALL=C find . -type f -name '*.*' -exec sed -i -e 's/#{wmVersionNumber}#/"+siteVariables['wmVersionNumber']+"/g' {} \\;", (err, stdout, stderr) => {
     if (err) {
         console.log("Command execution error:", err);
         return;
