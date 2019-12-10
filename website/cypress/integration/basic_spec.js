d33a427d7f5cd1c61915af7a63cacd275c1f925a
prasanth-reddy-wavemaker-com <57487155+prasanth-reddy-wavemaker-com@users.noreply.github.com>
2019-12-10 11:43:38 +0530

    Added searchbar related tests

--- website/cypress/integration/basic_spec.js
+++ website/cypress/integration/basic_spec.js
@@ -1,41 +1,59 @@
 describe('Top Navigation in web & mobile', function() {
-	
+  var nav = ".slidingNav", 
+	navSearchWrapper = ".navSearchWrapper",
+	learnAppUrl = "http://localhost:3000/learn/";
+  
   it('Verifies navigation elements in desktop', function() {
-    cy.visit("http://localhost:3000/learn/");
-	var nav = ".slidingNav";
+    cy.visit(learnAppUrl);
+	
 	var navElements = ["Docs", "Widgets", "How-to", "Releases"];
 	navElements.forEach(function(navItem) {
 		cy.get(nav).contains(navItem);
 	});
-	
   });
   
-  it('Verifies navigation elements in mobile', function() {
+  it("searchBar should be visible in desktop and mobile", function() { 
+	cy.visit(learnAppUrl);
+	
+	cy.get(nav).get(navSearchWrapper).should("be.visible");
 	cy.viewport('iphone-6');
-	var nav = ".slidingNav";
+	cy.get(nav).get(navSearchWrapper).should("be.visible");
+  });
+  
+  it('Verifies that only Docs, Search is visible in the navbar', function() {
+	cy.viewport('iphone-6');
+	cy.visit(learnAppUrl);
+	
 	var navElements = ["Docs"];
 	navElements.forEach(function(navItem) {
 		cy.get(nav).contains(navItem);
 	});
-	//Add assertion to check that only Docs is visible and not others in slidingNav and that search bar is visible.
+	
+	cy.get(nav).get(navSearchWrapper).should("be.visible");
+	
+	var hiddenNavElements = ["Widgets", "How-to", "Releases"];
+	hiddenNavElements.forEach(function(navItem) {
+		cy.get(nav).contains(navItem).should("not.be.visible");
+	});
   });
   
   it('Verifies that hamburger menu is visible in mobile', function() {
 	cy.viewport('iphone-6');
-	var nav = ".slidingNav";
-		
-	cy.get(nav).contains("Docs").click();
+	cy.visit(learnAppUrl);
+	
+	cy.get(nav).contains("Docs").click().end();
 	cy.get(".hamburger-menu").should("be.visible");
   });
 });
 
 describe("Login & Free trial button take to right places", function() {
+	var learnAppUrl = "http://localhost:3000/learn/";
 	it('Verifies navigation elements in desktop', function() {
-		cy.visit("http://localhost:3000/learn/");
+		cy.visit(learnAppUrl);
 		var nav = ".slidingNav";
 		
-		cy.get(nav).contains("Login").click();
-		cy.get(nav).contains("Start free trial").click();
+		cy.get(nav).contains("Login").click().end();
+		cy.get(nav).contains("Start free trial").click().end();
 		//cy.get(".main-container").contains("PLATFORM").click();
 	});
 });
