8133fa0621db22e4bb92267c7ec7fae388f7fbfe
Sairama B <sairama.bonala@wavemaker.com>
2019-12-26 12:16:45 +0530

    Changed names in script

--- website/changed_md_files.sh
+++ website/changed_md_files.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-my_array=()
+url_array=()
 while IFS= read -r line; do
-  my_array+=("$line")
+  url_array+=("$line")
 done < <(git diff --name-only HEAD HEAD~1 | grep '^.*[.md]$' | cut -d\. -f1 | cut -f2- -d '/')
 
 echo "[" > ./cypress/fixtures/changedFiles.json
-for ((i = 0; i < ${#my_array[@]}; i++)); do
-  if (( $i == ${#my_array[@]} -1 )) 
+for ((i = 0; i < ${#url_array[@]}; i++)); do
+  if (( $i == ${#url_array[@]} -1 )) 
   then
-    echo "\"${my_array[$i]}\"" >> ./cypress/fixtures/changedFiles.json
+    echo "\"${url_array[$i]}\"" >> ./cypress/fixtures/changedFiles.json
   else
-    echo "\"${my_array[$i]}\"," >> ./cypress/fixtures/changedFiles.json
+    echo "\"${url_array[$i]}\"," >> ./cypress/fixtures/changedFiles.json
   fi
 done
 echo "]" >> ./cypress/fixtures/changedFiles.json 
