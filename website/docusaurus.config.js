854c202fb51efd490c40c124a457e793d9d27c60
Swetha Kundaram <swethakundaram@gmail.com>
2023-02-17 21:44:45 +0530

    update home and links

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -75,7 +75,7 @@ module.exports = {
           "lastVersion": 'current',
           "versions": {
             "current": {
-              "label": 'v11.2',
+              "label": 'v11.2.4',
               "path": '',
             },
           },
