87e18562a01caf056ae4ef6987c1ecf030c42c64
Swetha Kundaram <swethakundaram@gmail.com>
2023-03-03 11:52:05 +0530

    sidebar update

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -70,7 +70,7 @@ export default class Index extends React.Component {
                     <div className='container banner spl-icon footer-left'>
                         <div className='row banner-content'>
                             <div className='col'>
-                                <h1 className='text--center text--semibold banner-Title'>How can we <span className='text--primary'>help</span>?</h1>
+                                <h1 className='text--center text--semibold banner-Title'>How can we help?</h1>
                                 <div className='banner-search' id="home-search">
                                 <svg width="20" height="20" class="DocSearch-Search-Icon" viewBox="0 0 20 20"><path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                                     <BrowserOnly>
