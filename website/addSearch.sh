4d9995b3a286c96c95af8b816b81b11a006aadc4
wavemaker_rn <wavemaker_rn@PRINHYLTAP1907.local>
2023-04-13 12:41:27 +0530

    updated script to add key config to docusaurus.config.js file

--- website/addSearch.sh
+++ website/addSearch.sh
@@ -5,6 +5,6 @@ else
 sed -i "" "s/        \/\/ placeholder={translations.placeholder}/        placeholder={translations.placeholder}/" src/theme/SearchBar/index.js
 sed -i "" "s/        \/\/ translations={translations.modal}/        translations={translations.modal}/" src/theme/SearchBar/index.js
 sed -i "" "s/\/\/ import translations from '@theme\/SearchTranslations';/import translations from '@theme\/SearchTranslations';/" src/theme/SearchBar/index.js
-sed -i "" 's/"@docusaurus\/preset-classic"/"@docusaurus\/preset-classic", \n\t\t\t{\n\t"googleAnalytics": {\n\t\t"trackingID": process.env.GA_TRACKING_ID,\n\t\t},\n\t}/' docusaurus.config.js
+sed  -e 'N' -e 's/"@docusaurus\/preset-classic",\n.*{/"@docusaurus\/preset-classic",\n\t{\n\t"googleAnalytics": {\n\t\t"trackingID": process.env.GA_TRACKING_ID,\n\t},/' docusaurus.config.js > data.json && mv data.json docusaurus.config.js
 sed -i "" 's/"mermaid": {/"algolia": {\n\t\t\t"appId": process.env.ALGOLIA_DS_APP_ID,\n\t\t\t"apiKey": process.env.ALGOLIA_DS_API_KEY,\n\t\t\t"indexName": process.env.ALGOLIA_DS_INDEX_NAME\n},\n"mermaid": {/' docusaurus.config.js
 fi
