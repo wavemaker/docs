9a0b2766a5ef06336c316ffd1de28d816952960c
rraajvamsy <raajvamsy.rayavarapu@wavemaker.com>
2023-08-20 15:27:09 +0530

    added recent search and updated test case

--- website/cypress/e2e/basic.cy.js
+++ website/cypress/e2e/basic.cy.js
@@ -46,55 +46,16 @@ describe("Top Navigation in web & mobile", function () {
     });
   });
 });
-describe("Sign-in button take to right places", function () {
-  var selectors = {
-    desktopNavElements: ".slidingNav > ul > li:not(:first-child)"
-  };
 
+describe("Verifies Get-Started button take to right places", function () {
   beforeEach(function () {
     cy.visit('/');
   });
 
-  it("Verifies Login navigation ", function () {
-
-    cy.get('.navbar__items > a:not(:first-child)')
-      .contains("Login").should('have.attr', 'href').then((href) => {
-        cy.origin(href, () => {
-          cy.visit('/', { 'failOnStatusCode': false });
-          cy.get(".signup-container").contains("Login to")
-            .end();
-        });
-      })
-  });
   it("Verifies Get Started navigation ", function () {
     cy.get('.navbar__items > a:last-child')
       .contains("Get Started").should('have.attr', 'href').then((href) => {
-        cy.origin(href, () => {
-          cy.visit('/', { 'failOnStatusCode': false });
-          cy.get(".ct-headline").contains("Start your free trial now")
-            .end();
+        cy.request(href).its('body').should('include', 'Start your free trial now').end();
         });
       })
-  })
-
-  // it("Verifies Footer navigation ", function () {
-  //   let footer = cy.get('.extra-links > ul > li')
-  //   footer.each((ele, index) => {
-  //     cy.get('.extra-links > ul > li')[index].find('a').should('have.attr', 'href').then((href) => {
-  //       cy.window().then((win) => {
-  //         cy.stub(win, 'open').as('windowOpen');
-  //         cy.origin(href, () => {
-  //           cy.visit('/', { 'failOnStatusCode': false });
-  //           cy.get("h1[class^='ct-headline']").each((element, ind) => {
-  //             if (ind == 1) {
-  //               cy.wrap(element).should('be.visible').contains("Talk To Us for Pricing")
-  //                 .end();
-  //             }
-  //           });
-  //           cy.go('back');
-  //         })
-  //       })
-  //     })
-  //   })
-  // })
 });
