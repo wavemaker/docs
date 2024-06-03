c67de73b4de62b19abe82e09308575b9e41c99f3
Swetha Kundaram <swethakundaram@gmail.com>
2024-06-04 00:26:07 +0530

    Release 11.7.2 (#911)
    * release notes 11.7.2
    
    * remove critical bug fixes as they are not updated
    
    * typo of blog
    
    * edits

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -77,7 +77,7 @@ module.exports = {
           "lastVersion": 'current',
           "versions": {
             "current": {
-              "label": 'v11.7.1',
+              "label": 'v11.7.2',
               "path": '',
             },
             "v10.14": {
