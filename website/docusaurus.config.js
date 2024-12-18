dcc6ed76a9ae20fa914ffbacebe74d2bb7364e1d
RitupL <ritupurna.lenka@wavemaker.com>
2024-12-18 14:24:47 +0530

    added release notes 11.9.4

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -77,7 +77,7 @@ module.exports = {
           "lastVersion": 'current',
           "versions": {
             "current": {
-              "label": 'v11.9.3',
+              "label": 'v11.9.4',
               "path": '',
             },
             "v10.14": {
