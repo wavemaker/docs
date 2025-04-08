97afdd63b385f71a624d99794f183d8798b775ff
Mayankp_500374 <mayankp_500374@PRINHYLTAP1204.local>
2025-04-08 17:43:04 +0530

    added flag component

---
+++ website/src/theme/MDXComponents.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import Flag from '@site/src/components/Flag';
+
+export default {
+  ...MDXComponents,
+  Flag,
+};
