d82dcb5e2abc61351e48282b7e8e0fb3e5a94bd3
mayankPrakashWavemaker <mayank.prakash@wavemaker.com>
2025-04-22 15:42:50 +0530

    added condition to ignore versioned doc in test

--- website/changed_md_files.sh
+++ website/changed_md_files.sh
@@ -2,7 +2,7 @@
 url_array=()
 while IFS= read -r line; do
   url_array+=("$line")
-done < <(git diff --name-status --diff-filter=d HEAD~1 HEAD | grep '^.*[.md]$' | awk '{print $(NF)}' | cut -d\. -f1 | cut -f2- -d '/')
+done < <(git diff --name-status --diff-filter=d HEAD~1 HEAD | grep '^.*[.md]$' | grep -v 'versioned_docs' | awk '{print $(NF)}' | cut -d\. -f1 | cut -f2- -d '/')
 
 echo "[" > ./cypress/fixtures/changedFiles.json
 for ((i = 0; i < ${#url_array[@]}; i++)); do
