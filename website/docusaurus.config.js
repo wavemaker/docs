7726132ebd3fc225b890f0f339fe0e9f1e2ea478
mayankPrakashWavemaker <mayank.prakash@wavemaker.com>
2025-06-09 11:18:09 +0530

    realease notes 11.11.3 updated for release

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -77,7 +77,7 @@ module.exports = {
           "lastVersion": 'current',
           "versions": {
             "current": {
-              "label": 'v11.11.2',
+              "label": 'v11.11.3',
               "path": '',
             },
             "v10.14": {
