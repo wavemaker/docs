90a6d0a385cd5170ab1430b178f7edb018b0f2b5
mayankPrakashWavemaker <mayank.prakash@wavemaker.com>
2025-11-03 11:54:11 +0530

    updated version to 11.14.1

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -77,7 +77,7 @@ module.exports = {
           "lastVersion": 'current',
           "versions": {
             "current": {
-              "label": 'v11.14.0',
+              "label": 'v11.14.1',
               "path": '',
             },
             "v10.14": {
