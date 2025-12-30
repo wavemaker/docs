12beca5e342190ca1556afee9d9a79c0b0056ef6
mayankPrakashWavemaker <mayank.prakash@wavemaker.com>
2025-12-30 13:15:13 +0530

    guide sidebar cleanup

--- sidebar/sidebars/guideSidebar.js
+++ sidebar/sidebars/guideSidebar.js
@@ -1,10 +1,6 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarConfig} */
 export default [
     {
-        "type": "doc",
-        "id": "guide/intro"
-    },
-    {
         "type": "category",
         "label": "How-tos",
         "items": [
