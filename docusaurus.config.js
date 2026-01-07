0eff572998e69bab95ee5557cf06dd98fbeffe84
mayankPrakashWavemaker <mayank.prakash@wavemaker.com>
2026-01-07 14:07:48 +0530

    enabled rspack bundler

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,7 @@ const config = {
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    experimental_faster: true,
   },
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
