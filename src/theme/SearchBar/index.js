44357bc3ad51d996c1df155411ce7cafc0ccdb10
Tiwariji-07 <vivek.raj@wavemaker.com>
2026-02-04 16:14:51 +0530

    Video ui

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -397,11 +397,19 @@ function AskAIPanel({ query, apiUrl, onClose, isVisible, triggerSearch }) {
                                     target="_blank"
                                     rel="noopener noreferrer"
                                 >
-                                    <svg className="DocSearch-AskAI-VideoCard-Icon" width="18" height="18" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M4 4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H16C17.1 20 18 19.1 18 18V15L22 19V5L18 9V6C18 4.9 17.1 4 16 4H4ZM4 6H16V18H4V6Z" />
+                                    <div className="DocSearch-AskAI-VideoCard-Content">
+                                        <div className="DocSearch-AskAI-VideoCard-Title">{v.title}</div>
+                                        <div className="DocSearch-AskAI-VideoCard-Subtitle">Watch for deeper insights</div>
+                                    </div>
+                                    <svg className="DocSearch-AskAI-VideoCard-PlayIcon" viewBox="0 0 24 24" fill="currentColor">
+                                        <path d="M8 5v14l11-7z" />
                                     </svg>
-                                    <span className="DocSearch-AskAI-VideoCard-Title">{v.title}</span>
-                                    <span className="DocSearch-AskAI-VideoCard-Play">▶</span>
+                                    <div className="DocSearch-AskAI-VideoCard-Badge">
+                                        <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor">
+                                            <path d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2-1.09V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72L12 15l5-2.73v3.72z" />
+                                        </svg>
+                                        Academy
+                                    </div>
                                 </a>
                             ))}
                         </div>
